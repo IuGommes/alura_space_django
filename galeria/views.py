5c7a036d2410953660d5dbaa442287375de9fb94
Iuri Gomes <iurim31@gmail.com>
2024-03-13 11:10:29 -0300

    atualizacao

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.order_by("data_publicacao").filter(publicado=True)
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data_publicacao").filter(publicado=True)
+
+    if "buscar" in request.GET:
+        nome_pesquisado = request.GET['buscar']
+        if nome_pesquisado:
+            fotografias = fotografias.filter(nome__icontains=nome_pesquisado)
+
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
